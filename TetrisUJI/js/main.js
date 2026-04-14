86dbb1559d7b14af7c37f83d77591d32dea232cf
iLuis24 <113622960+iLuis24@users.noreply.github.com>
2026-04-14 19:23:48 +0200

    Update main.js

--- TetrisUJI/js/main.js
+++ TetrisUJI/js/main.js
@@ -1,4 +1,22 @@
 let game = new Phaser.Game(gameWidth, gameHeight, Phaser.AUTO, 'game');
 
+let wfConfig = {
+    active: function () {
+        startGame();
+    },
+
+    google: {
+        families: ['Rammetto One', 'Sniglet']
+    },
+
+    custom: {
+        // Your Turn 3
+        families: ['FerrumExtracondensed'],
+        urls: ["https://fontlibrary.org/face/ferrum"]
+    }
+};
+
+WebFont.load(wfConfig);
+
 game.state.add('Game', gameState);
 game.state.start('Game');
