da6086a6cc06ae6c7a9864b012687ea5de588e7c
iLuis24 <113622960+iLuis24@users.noreply.github.com>
2026-04-22 20:51:34 +0200

    cambio en codigo correccion

--- TetrisUJI/js/main.js
+++ TetrisUJI/js/main.js
@@ -1,24 +1,19 @@
-// states(pantallas) el main último en arrancar
-let game = new Phaser.Game(gameWidth, gameHeight, Phaser.AUTO, 'game');
 
-game.state.add('Splash', splashState);
-game.state.add('Menu', menuState);
-game.state.add('Levels', levelsState);
-game.state.add('Game', gameState);
-game.state.add('HallFame', hallFameState);
-game.state.add('credits', creditsState);
+let game;
 
-function startGame() {
-	game.state.start('Splash');
-}
+window.onload = function () {
 
-if (document.fonts && document.fonts.load) {
-	Promise.all([
-		document.fonts.load('48px KyotoTitle'),
-		document.fonts.load('22px MangaStyle'),
-		document.fonts.load('18px MangaStyle'),
-		document.fonts.load('28px MangaStyle')
-	]).then(startGame).catch(startGame);
-} else {
-	window.setTimeout(startGame, 100);
-}
+    // crear juego
+    game = new Phaser.Game(gameWidth, gameHeight, Phaser.AUTO, 'game');
+
+    // añadir states (pantallas)
+    game.state.add('Splash', splashState);
+    game.state.add('Menu', menuState);
+    game.state.add('Levels', levelsState);
+    game.state.add('Game', gameState);
+    game.state.add('HallFame', hallFameState);
+    game.state.add('credits', creditsState);
+
+    // iniciar juego
+    game.state.start('Splash');
+};
