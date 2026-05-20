dd620cc11ceb88915ba28494403845c632fb5852
iLuis24 <113622960+iLuis24@users.noreply.github.com>
2026-05-20 12:26:58 +0200

    wind

--- TetrisUJI/js/splash.js
+++ TetrisUJI/js/splash.js
@@ -1,6 +1,6 @@
 var splashState = {
   preload: function () {
-    game.load.image('windArm', 'assets/BG/brazo.png');
+    game.load.image('wind', 'assets/BG/wind.png');
     game.load.audio('Button1', 'assets/sound/Button1.mp3');
     game.load.audio('Button2', 'assets/sound/Button2.mp3');
     game.load.audio('Eating', 'assets/sound/Eating.mp3');
@@ -13,6 +13,7 @@ var splashState = {
     game.load.audio('Pop', 'assets/sound/Pop.mp3');
     game.load.audio('Ticktock', 'assets/sound/ticktock.mp3');
     game.load.audio('NoRotation', 'assets/sound/no_rotation.mp3');
+    game.load.audio('wind', 'assets/sound/wind.mp3');
   },
 
   create: function () {
